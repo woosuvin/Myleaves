632abcce44feef68f0f636ebf51030597600b293
SuJeKim <gjimin26@gmail.com>
2023-07-31 15:00:08 +0900

    test commit - jimin

--- src/main/java/com/itwill/myleaves/MyleavesApplication.java
+++ src/main/java/com/itwill/myleaves/MyleavesApplication.java
@@ -4,13 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import lombok.extern.slf4j.Slf4j;
 
+
+@Slf4j
 @EnableJpaAuditing 
 @SpringBootApplication
 public class MyleavesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyleavesApplication.class, args);
+		log.info("main started");
 	}
 
 }
