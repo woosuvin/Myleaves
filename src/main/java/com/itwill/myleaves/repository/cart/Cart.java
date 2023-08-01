aa30d402d25b9707395d191a1dd665d23ee50b2e
itwill <itwill@DESKTOP-T787U4E>
2023-08-01 19:08:18 +0900

    cart.java 수정

--- src/main/java/com/itwill/myleaves/repository/cart/Cart.java
+++ src/main/java/com/itwill/myleaves/repository/cart/Cart.java
@@ -1,36 +1,34 @@
-package com.itwill.myleaves.repository.cart;
-
-import java.time.LocalDateTime;
-
-import org.springframework.data.annotation.CreatedDate;
-
-import jakarta.persistence.Column;
-import jakarta.persistence.EmbeddedId;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
-
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
-@Getter
-@ToString
-
-@Entity
-@Table(name = "CART")
-public class Cart {
-	
-	@EmbeddedId
-	private CartId cartId;
-	
-	@Column(nullable = true)
-	private long cnt;
-	
-	@CreatedDate
-	private LocalDateTime addDate;
-
-}
+/*
+ * package com.itwill.myleaves.repository.cart;
+ * 
+ * import java.time.LocalDateTime;
+ * 
+ * import org.springframework.data.annotation.CreatedDate;
+ * 
+ * import jakarta.persistence.Column; import jakarta.persistence.EmbeddedId;
+ * import jakarta.persistence.Entity; import jakarta.persistence.Table; import
+ * lombok.AllArgsConstructor; import lombok.Builder; import lombok.Getter;
+ * import lombok.NoArgsConstructor; import lombok.ToString;
+ * 
+ * @NoArgsConstructor
+ * 
+ * @AllArgsConstructor
+ * 
+ * @Builder
+ * 
+ * @Getter
+ * 
+ * @ToString
+ * 
+ * @Entity
+ * 
+ * @Table(name = "CART") public class Cart {
+ * 
+ * @EmbeddedId private CartId cartId;
+ * 
+ * @Column(nullable = true) private long cnt;
+ * 
+ * @CreatedDate private LocalDateTime addDate;
+ * 
+ * }
+ */
