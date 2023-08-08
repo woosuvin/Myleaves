6f89940cd9718ff7746b9d16fb4d2cbd91b84362
rosy137(Blair) <rosybb13@gmail.com>
2023-08-08 18:59:28 +0900

    지현 결제 페이지 포맷, 스토어 디테일, 분양 디테일 포맷

--- src/main/java/com/itwill/myleaves/web/mngr/MngrOrderController.java
+++ src/main/java/com/itwill/myleaves/web/mngr/MngrOrderController.java
@@ -1,9 +1,15 @@
 package com.itwill.myleaves.web.mngr;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.itwill.myleaves.dto.order.TotalOrderUpdateDto;
+import com.itwill.myleaves.repository.totalOrder.TotalOrder;
+import com.itwill.myleaves.service.totalOrder.TotalOrderService;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -14,10 +20,13 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping("/mngr")
 public class MngrOrderController {
 
-	@GetMapping("/order")
+	private final TotalOrderService totalOrderService;
+	
+	@GetMapping("/order/list")
 	public void mngrHome(Model model) {
 		log.info("mngr order");
-		
+		List<TotalOrder> list = totalOrderService.read();
+		model.addAttribute("totalOrders", list);
 	}
 	
 	
