505db566700bae4d7af0947c957f08c095f00144
baesunyoung34 <baesaa0304@nvaer.com>
2023-08-11 18:15:45 +0900

    배선영 QnA FaQ CSS 중

--- src/main/java/com/itwill/myleaves/web/info/QnAController.java
+++ src/main/java/com/itwill/myleaves/web/info/QnAController.java
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-
 import com.itwill.myleaves.dto.qna.QnACreateDto;
 import com.itwill.myleaves.dto.qna.QnASearchAndPageDto;
 import com.itwill.myleaves.dto.qna.QnAUpdateDto;
@@ -24,117 +23,145 @@ import com.itwill.myleaves.service.qna.QnAService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
-
 @Slf4j
 @RequiredArgsConstructor
 @Controller
 @RequestMapping("/info/qna")
 public class QnAController {
-	
+
 	private final QnAService qnaService;
-	
+
 	/*
 	 * QnA 리스트 메인 페이지
 	 */
-	@PreAuthorize("hasRole('MEMBER')")
+	/*
+	 * @PreAuthorize("hasRole('MEMBER')")
+	 * 
+	 * @GetMapping public String readQnA(Model model , @PageableDefault(page=0,
+	 * size=5, sort="qid", direction=Sort.Direction.DESC) Pageable pageable) {
+	 * log.info("QNA read()");
+	 * 
+	 * Page<QnA> list = qnaService.read(pageable);
+	 * 
+	 * int nowPage = list.getPageable().getPageNumber() + 1; // 현재페이지 int startPage
+	 * = Math.max(nowPage - 4, 1); // 시작 페이지 int endPage = Math.min(nowPage + 5,
+	 * list.getTotalPages()); // 끝 페이지
+	 * 
+	 * model.addAttribute("qnas" , list); model.addAttribute("nowPage",nowPage);
+	 * model.addAttribute("startPage", startPage); model.addAttribute("endPage",
+	 * endPage);
+	 * 
+	 * return "info/qna/read"; }
+	 */
 	@GetMapping
-	public String readQnA(Model model , @PageableDefault(page=0, size=10, sort="qid", direction=Sort.Direction.DESC) Pageable pageable) {
-		log.info("QNA read()");
-		
+	public String qnaList(Model model,
+			@PageableDefault(page = 0, size = 10, sort = "qid", direction = Sort.Direction.DESC) Pageable pageable) {
+		log.info("QnA My list()");
+
 		Page<QnA> list = qnaService.read(pageable);
-		
+
 		int nowPage = list.getPageable().getPageNumber() + 1; // 현재페이지
-        int startPage =  Math.max(nowPage - 4, 1); // 시작 페이지
-        int endPage = Math.min(nowPage +5, list.getTotalPages()); // 끝 페이지
-       
-        model.addAttribute("qnas" , list);
-        model.addAttribute("nowPage",nowPage);
-        model.addAttribute("startPage", startPage);
-        model.addAttribute("endPage", endPage);
-       
+		int maxPage = list.getTotalPages();
+
+		int visiblePageCount = 5; // 한 번에 표시될 페이지 번호의 갯수
+		int halfVisiblePageCount = visiblePageCount / 2;
+
+		int startPage = Math.max(nowPage - halfVisiblePageCount, 1);
+		int endPage = Math.min(startPage + visiblePageCount - 1, maxPage);
+
+		model.addAttribute("qnas", list);
+		model.addAttribute("nowPage", nowPage);
+		model.addAttribute("startPage", startPage);
+		model.addAttribute("endPage", endPage);
+
 		return "info/qna/read";
 	}
-	
+
 	/*
 	 * QnA 검색 기능
+	 * 
+	 * @PreAuthorize("hasRole('MEMBER')")
+	 * 
+	 * @GetMapping("/search") public String searchQnA(QnASearchAndPageDto dto, Model
+	 * model, @PageableDefault(page=0, size=10, sort="qid",
+	 * direction=Sort.Direction.DESC) Pageable pageable) { log.info("search(dto={})"
+	 * , dto);
+	 * 
+	 * Page<QnA> list = qnaService.searchQnA(dto , pageable);
+	 * 
+	 * int nowPage = list.getPageable().getPageNumber() + 1; // 현재페이지 int maxPage =
+	 * list.getTotalPages();
+	 * 
+	 * int visiblePageCount = 5; // 한 번에 표시될 페이지 번호의 갯수 int halfVisiblePageCount =
+	 * visiblePageCount / 2;
+	 * 
+	 * int startPage = Math.max(nowPage - halfVisiblePageCount, 1); int endPage =
+	 * Math.min(startPage + visiblePageCount - 1, maxPage);
+	 * 
+	 * model.addAttribute("qnas", list); model.addAttribute("nowPage", nowPage);
+	 * model.addAttribute("startPage", startPage); model.addAttribute("endPage",
+	 * endPage);
+	 * 
+	 * return "info/qna/read"; }
 	 */
-	@PreAuthorize("hasRole('MEMBER')")
-	@GetMapping("/search")
-	public String searchQnA(QnASearchAndPageDto dto, Model model, @PageableDefault(page=0, size=10, sort="qid", direction=Sort.Direction.DESC) Pageable pageable) {
-		log.info("search(dto={})" , dto);
-		
-		Page<QnA> list = qnaService.searchQnA(dto , pageable);
-		
-		 int nowPage = list.getPageable().getPageNumber() + 1;
-		 int startPage = Math.max(nowPage - 4, 1);
-		 int endPage = Math.min(nowPage + 9, list.getTotalPages());
-		
-		model.addAttribute("qnas" , list);
-		model.addAttribute("startPage", startPage);
-	    model.addAttribute("endPage", endPage);
-	    model.addAttribute("nowPage", nowPage);
-		
-		return "info/qna/read";
-	}
 
 	/*
 	 * QnA 새 글 작성하기
 	 */
 	@PreAuthorize("hasRole('MEMBER')")
 	@GetMapping("/create")
-    public void create() {
-        log.info("QNA create() GET");
-        
+	public void create() {
+		log.info("QNA create() GET");
+
 	}
+
 	@PreAuthorize("hasRole('MEMBER')")
 	@PostMapping("/create")
 	public String create(QnACreateDto dto) {
-		log.info("QnA create(dto={}) POST" , dto);
-		
+		log.info("QnA create(dto={}) POST", dto);
+
 		qnaService.create(dto);
-		
+
 		return "redirect:/info/qna";
 	}
-	
-	
+
 	/*
 	 * QnA 상세 페이지 QnA 수정 페이지
 	 */
 	@PreAuthorize("hasRole('MEMBER')")
-	@GetMapping({"/detail", "/modify"})
+	@GetMapping({ "/detail", "/modify" })
 	public void detailQnA(Long qid, Model model) {
-		log.info("QnA read(id={})" , qid);
-		
+		log.info("QnA read(id={})", qid);
+
 		QnA qna = qnaService.read(qid);
-		
-		model.addAttribute("qna" , qna);
+
+		model.addAttribute("qna", qna);
 	}
+
 	/*
 	 * QnA 수정 보내기
 	 */
 	@PreAuthorize("hasRole('MEMBER')")
 	@PostMapping("/update")
 	public String update(QnAUpdateDto dto) {
-        log.info("update dto={}" , dto);
-        
-        qnaService.update(dto);
-        
-        return "redirect:/info/qna/detail?qid=" + dto.getQid();
+		log.info("update dto={}", dto);
+
+		qnaService.update(dto);
+
+		return "redirect:/info/qna/detail?qid=" + dto.getQid();
 	}
-	
+
 	/*
 	 * QnA 삭제 페이지
 	 */
 	@PreAuthorize("hasRole('MEMBER')")
-	 @PostMapping("/delete")    
-	    public String delete(long qid) {
-	        log.info("delete(id={})" , qid);
-	   
-	        qnaService.delete(qid);
-	        
-	        return "redirect:/info/qna";
-	    }
-	
-	
+	@PostMapping("/delete")
+	public String delete(long qid) {
+		log.info("delete(id={})", qid);
+
+		qnaService.delete(qid);
+
+		return "redirect:/info/qna";
+	}
 
 }
